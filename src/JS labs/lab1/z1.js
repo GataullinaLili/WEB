18c650d61f63c769de62ccf333548c1a29593a49
GataullinaLILI <liliag00@mail.ru>
2025-04-14 19:25:14 +0300

    add z1 in lab 1 JS

---
+++ src/JS labs/lab1/z1.js
@@ -0,0 +1,14 @@
+function grade (a)
+{
+if (a>0)
+{console.log("Младенец")}
+else if(a<=13)
+{console.log("Ребенок")}
+else if(a<=19)
+{console.log("Подросток")}
+else if(a<=65)
+{console.log("Взрослый")}
+else
+{console.log("Пожилой")}
+}
+grade(20, 1, 100)
