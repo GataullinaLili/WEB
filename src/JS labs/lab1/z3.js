ab38b8dd2aee9f409111ffd0d1c2b828a4664265
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2025-04-15 19:27:19 +0300

    Update z3.js

--- src/JS labs/lab1/z3.js
+++ src/JS labs/lab1/z3.js
@@ -1,21 +1,21 @@
 function check(number) {
 
   if (number % 10 === 0) {
-    return "Число делится на 10";
+    console.log("Число делится на 10");
   } else if (number % 6 === 0) {
-    return "Число делится на 6";
+    console.log("Число делится на 6");
   } else if (number % 3 === 0) {
-    return "Число делится на 3";
+    console.log("Число делится на 3");
   } else if (number % 7 === 0) {
-    return "Число делится на 7";
+    console.log("Число делится на 7");
   } else if (number % 2 === 0) {
-    return "Число делится на 2";
+   console.log("Число делится на 2");
   } else {
-    return "Не делится";
+   console.log("Не делится");
   }
 }
 
-console.log(check(30));   // Число делится на 10
-console.log(check(15));   //  Число делится на 3
-console.log(check(12));    // Число делится на 6
-console.log(check(1643));   // Не делится
+check(30);   // Число делится на 10
+check(15);   //  Число делится на 3
+check(12);    // Число делится на 6
+check(1643);   // Не делится
