3b4609f050d4e39a8ade57be305ce38a01cd5b63
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2025-05-13 19:43:09 +0300

    Update 1.js

--- src/JS labs/lab5/1.js
+++ src/JS labs/lab5/1.js
@@ -1,1 +1,19 @@
+const personals = [
+'Silas Butler',
+'Adnaan Buckley',
+'Juan Peterson',
+'Brendan Villarreal'
+];
+
+const personal = {};
+
+for (let i = 0; i < personals.length; i++) {
+    const name = personals[i];
+    const personalNum = name.length; 
+    personal[name] = personalNum; 
+}
+
+for (const [employeeName, personalNum] of Object.entries(personal)) {
+    console.log(`Name: ${employeeName} - Personal Number: ${personalNum}`);
+}
 
