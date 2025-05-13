cda86a0993678fbc69903d1eb38963fd29998732
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2025-05-13 15:22:12 +0300

    Update subtract.js in lab 6/1

--- src/JS labs/lab6/1/subtract.js
+++ src/JS labs/lab6/1/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    const firstNumber = parseFloat(document.getElementById('firstNumber').value);
+    const secondNumber = parseFloat(document.getElementById('secondNumber').value);
+    const result = firstNumber - secondNumber;
+    document.getElementById('result').textContent = result;
 }
