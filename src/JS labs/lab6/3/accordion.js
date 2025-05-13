f948c524b82688837c860e629c485370e15bf750
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2025-05-13 15:33:58 +0300

    Update accordion.js 6/3

--- src/JS labs/lab6/3/accordion.js
+++ src/JS labs/lab6/3/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    const more = document.getElementById('extra');
+    const button = document.querySelector('.button');
+    if (more.style.display === 'none' || more.style.display === '') {
+        more.style.display = 'block';
+        button.textContent = 'Less';
+    } else {
+        more.style.display = 'none';
+        button.textContent = 'More';
+    }
 }
