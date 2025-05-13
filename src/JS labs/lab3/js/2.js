2280dd510a8599d23c111d4681e11c5070325bf9
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2025-05-13 14:54:31 +0300

    Update 2.js

--- src/JS labs/lab3/js/2.js
+++ src/JS labs/lab3/js/2.js
@@ -1,16 +1,7 @@
 function el(arr1, arr2) {
-    
-    for (let i=0; i<arr1.length;i++){
-        arr1[i]=Number(arr1[i]);
-    }
-    for (let i=0; i<arr2.length;i++){
-        arr2[i]=Number(arr2[i]);
-    }
-
-    for(let i =0; i < arr1.length;i++)
-    {if (arr1[i] === arr2[i])
-        console.log(arr1[i]);
-    }
-    
+    let a = arr1.filter(item => arr2.includes(item));
+    console.log(a.join("\n"));
 }
-el(['Hey','hello',2, 4,'Peter','e'], ['Petar','hey',10, 4,'hello','2']);
+
+el(['Hey', 'hello', 2, 4, 'Peter', 'e'], ['Petar', 'hey', 10, 4, 'hello', '2']);
+el(['R', 'u', 's', 's', 'i', 'a'], ['R', 'u', 't']);
