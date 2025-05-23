314e6a2df4c9f0572e9c5554fbcd7915db799862
GataullinaLili <127747080+GataullinaLili@users.noreply.github.com>
2025-05-23 10:32:12 +0300

    Update 2.js

--- src/JS labs/lab5/2.js
+++ src/JS labs/lab5/2.js
@@ -1,21 +1,24 @@
-function towns(data) {
-        for (let i = 1; i < data.length; i++) {
-            const [town, latitude, longitude] = data[i]
-                .split(' | ')
-                .map(x => x.trim());
-    
-            const townObj = {
-                town,
-                latitude: Number(latitude).toFixed(2),
-                longitude: Number(longitude).toFixed(2)
-            };
-    
-            console.log(`"Город | Широта | Долгота"`,townObj);
-        }
-    }
-    
-towns([
-    
-        "Москва | 55.7522 | 37.6156",
-        "Beijing | 39.913818 | 116.363625",
-    ]);
+function Table(data) {
+    return data.map(row => {
+        const [town, latStr, lonStr] = row.split('|').map(item => item.trim());
+
+        const latitude = parseFloat(latStr);
+        const longitude = parseFloat(lonStr);
+
+        const fLat = latitude.toFixed(2);
+        const fLon = longitude.toFixed(2);
+
+        return {
+            town: town,
+            latitude: fLat,
+            longitude: fLon
+        };
+    });
+}
+
+const a = [
+    'Moscow | 55.7522 | 37.6156',
+    'Beijing | 39.913818 | 116.363625'
+];
+
+Table(a).forEach(t => console.log(t));
